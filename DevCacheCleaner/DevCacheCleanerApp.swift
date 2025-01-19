0bd3475c9cef8ac669c8cf5a31d5e3fd9f535a39
Alex Santeeno <alexsanteeno@gmail.com>
2025-01-19 20:41:14 +0100

    Initial Commit

---
+++ DevCacheCleaner/DevCacheCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCacheCleanerApp.swift
+//  DevCacheCleaner
+//
+//  Created by Oleksandr Hanhaliuk on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct DevCacheCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevCacheCleanerTests/DevCacheCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DevCacheCleanerTests.swift
+//  DevCacheCleanerTests
+//
+//  Created by Oleksandr Hanhaliuk on 19/01/2025.
+//
+
+import Testing
+@testable import DevCacheCleaner
+
+struct DevCacheCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
